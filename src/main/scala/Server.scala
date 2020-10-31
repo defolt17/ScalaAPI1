6c874822ac347c412063cdbd702fbadbb8b9945b
defolt17 <defolt_17@mail.ru>
2020-10-31 13:23:29 +0300

    transfer point

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,4 +1,4 @@
-import Main.materializer.system
+import Main.{host, port, system}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.javadsl.ServerBinding
@@ -6,6 +6,6 @@ import akka.stream.ActorMaterializer
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class Server(router: Router, host: String, port Int)(implicit system: ActorSystem, ex: ExecutionContext, mat: ActorMaterializer) {
-    def bind(): Future[ServerBinding] = Http().bindAndHandle
+class Server(router: Router, host: String, port: Int)(implicit system: ActorSystem, ex: ExecutionContext, mat: ActorMaterializer) {
+    def bind(): Future[Http.ServerBinding] = Http().bindAndHandle(router.route, host, port)
 }
